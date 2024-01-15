cb1ca1d95b79a54aa4f1d3bb6084a033ae48b2b9
RtxIon <romangorin28@gmail.com>
2024-01-14 17:32:31 -0800

    sorting out collision

--- Pong/Form1.cs
+++ Pong/Form1.cs
@@ -2,6 +2,31 @@ namespace Pong
 {
     public partial class Form1 : Form
     {
+        bool intersects(Rectangle a, Rectangle b)
+        {
+            // a is ball |  b is paddle
+            if (a.Left <= b.Right || a.Right >= b.Left  && (a.Top <= b.Bottom && a.Bottom >= b.Bottom || a.Bottom >= b.Top && a.Top <= b.Top))
+            {
+                gfx.FillRectangle(Brushes.Crimson, leftPaddle);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        //bool intersectsRight(Rectangle a, Rectangle b)
+        //{
+        //    if  && (a.Top <= b.Bottom && a.Bottom >= b.Bottom || a.Bottom >= b.Top && a.Top <= b.Top))
+        //    {
+        //        gfx.FillRectangle(Brushes.Aquamarine, rightPaddle);
+        //        return true;
+        //    }
+        //    else
+        //    {
+        //        return false;
+        //    }
+        //}
         Graphics gfx;
         Bitmap bmp;
         Rectangle leftPaddle;
@@ -11,9 +36,10 @@ namespace Pong
         public Form1()
         {
 
-            leftPaddle = new Rectangle(0,0,60, 120);
-            rightPaddle = new Rectangle(300, 300, 50, 40);
-            ball = new Rectangle(80, 80, 30, 30);
+            leftPaddle = new Rectangle(0,0,50, 120);
+            rightPaddle = new Rectangle(750, 120, 50, 120);
+            //ball = new Rectangle(190, 80, 30, 30);
+            ball = new Rectangle(720, 150, 30, 30);
             InitializeComponent();
 
             bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
@@ -30,8 +56,8 @@ namespace Pong
             gfx.FillRectangle(Brushes.Chocolate, leftPaddle);
             gfx.FillRectangle(Brushes.BurlyWood, rightPaddle);
             gfx.FillEllipse(Brushes.DarkMagenta, ball);
-            ball.X += speed.X;
-            ball.Y += speed.Y;
+            ball.X += 0;//speed.X;
+            ball.Y += 0;//speed.Y;
 
             // walls
             if (ball.Y <= 0 || ball.Y + ball.Height >= ClientSize.Height)
@@ -43,21 +69,34 @@ namespace Pong
                 speed.X *= -1;
                 // flip player off
             }
+            if (intersects(ball, leftPaddle))
+            {
+             
+                speed.X = Math.Abs(speed.X);
+            }
+            if (intersects(ball, rightPaddle))
+            {
+                
+                speed.X = -Math.Abs(speed.X);
+            }
             // paddles  
-            if(ball.X <= leftPaddle.Right)
-            {
-                speed.X *= -1;
-            }
-            if(ball.Y <= leftPaddle.Height)
-            {
-                speed.Y *= -1;
-            }
+            //if(ball.X <= leftPaddle.Right)
+            //{
+            //    speed.X *= -1;
+            //}
+            //if(ball.Y <= leftPaddle.Height)
+            //{
+            //    //speed.Y *= -1;
+            //}
+
+
 
 
 
 
             pictureBox1.Image = bmp;
         }
+
 
         private void Form1_KeyDown(object sender, KeyEventArgs e)
         {
