a032cba5ff0ff6e833718baabc6ee9b1bf14a028
RtxIon <romangorin28@gmail.com>
2024-01-21 17:32:33 -0800

    wrapping up

--- Pong/Form1.Designer.cs
+++ Pong/Form1.Designer.cs
@@ -31,6 +31,8 @@
             this.components = new System.ComponentModel.Container();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.UpdateTimer = new System.Windows.Forms.Timer(this.components);
+            this.score_text_box = new System.Windows.Forms.TextBox();
+            this.score_text_textbox = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -48,18 +50,41 @@
             this.UpdateTimer.Interval = 17;
             this.UpdateTimer.Tick += new System.EventHandler(this.UpdateTimer_Tick);
             // 
+            // score_text_box
+            // 
+            this.score_text_box.Enabled = false;
+            this.score_text_box.Location = new System.Drawing.Point(337, 62);
+            this.score_text_box.Name = "score_text_box";
+            this.score_text_box.Size = new System.Drawing.Size(100, 23);
+            this.score_text_box.TabIndex = 1;
+            this.score_text_box.Text = "            0 | 0";
+            // 
+            // score_text_textbox
+            // 
+            this.score_text_textbox.Enabled = false;
+            this.score_text_textbox.Location = new System.Drawing.Point(295, 12);
+            this.score_text_textbox.Multiline = true;
+            this.score_text_textbox.Name = "score_text_textbox";
+            this.score_text_textbox.Size = new System.Drawing.Size(188, 44);
+            this.score_text_textbox.TabIndex = 2;
+            this.score_text_textbox.Text = "Score:";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 499);
+            this.Controls.Add(this.score_text_textbox);
+            this.Controls.Add(this.score_text_box);
             this.Controls.Add(this.pictureBox1);
             this.KeyPreview = true;
             this.Name = "Form1";
             this.Text = "Form1";
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -67,5 +92,7 @@
 
         private PictureBox pictureBox1;
         private System.Windows.Forms.Timer UpdateTimer;
+        private TextBox score_text_box;
+        private TextBox score_text_textbox;
     }
 }
--- Pong/Form1.cs
+++ Pong/Form1.cs
@@ -1,3 +1,5 @@
+using System.Net.Http.Headers;
+
 namespace Pong
 {
     public partial class Form1 : Form
@@ -5,9 +7,10 @@ namespace Pong
         bool intersects(Rectangle a, Rectangle b)
         {
             // a is ball |  b is paddle
-            if (a.Left <= b.Right || a.Right >= b.Left  && (a.Top <= b.Bottom && a.Bottom >= b.Bottom || a.Bottom >= b.Top && a.Top <= b.Top))
+            //a.Left <= b.Right || a.Right >= b.Left && (a.Top <= b.Bottom && a.Bottom >= b.Bottom || a.Bottom >= b.Top && a.Top <= b.Top)
+            if (ball.IntersectsWith(b))
             {
-                gfx.FillRectangle(Brushes.Crimson, leftPaddle);
+                gfx.FillRectangle(Brushes.Crimson, b);
                 return true;
             }
             else
@@ -33,14 +36,18 @@ namespace Pong
         Rectangle rightPaddle;
         Rectangle ball;
         Point speed = new Point(5, 5);
+        int winning_score = 10;
+        int l_score = 0;
+        int r_score = 0;
+        HashSet<Keys> pressedKeys;
         public Form1()
         {
-
+            InitializeComponent();
+            score_text_box.Text = $"            {l_score} | {r_score}";
             leftPaddle = new Rectangle(0,0,50, 120);
             rightPaddle = new Rectangle(750, 120, 50, 120);
-            //ball = new Rectangle(190, 80, 30, 30);
-            ball = new Rectangle(720, 150, 30, 30);
-            InitializeComponent();
+            ball = new Rectangle(190, 80, 30, 30);
+            //ball = new Rectangle(720, 150, 30, 30);
 
             bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
 
@@ -52,47 +59,76 @@ namespace Pong
         private void UpdateTimer_Tick(object sender, EventArgs e) // loop
         {
             gfx.Clear(Color.WhiteSmoke);
-            
+
             gfx.FillRectangle(Brushes.Chocolate, leftPaddle);
             gfx.FillRectangle(Brushes.BurlyWood, rightPaddle);
             gfx.FillEllipse(Brushes.DarkMagenta, ball);
-            ball.X += 0;//speed.X;
-            ball.Y += 0;//speed.Y;
+            ball.X += speed.X;//speed.X;
+            ball.Y += speed.Y;//speed.Y;
 
             // walls
-            if (ball.Y <= 0 || ball.Y + ball.Height >= ClientSize.Height)
+            if (ball.Top <= 0 || ball.Bottom >= ClientSize.Height)
             {
                 speed.Y *= -1;
             }
-            if (ball.X <= 0 || ball.Right >= ClientSize.Width)
+            if (ball.Left <= 0)
             {
                 speed.X *= -1;
+                r_score++;
                 // flip player off
+            }
+            else if (ball.Right >= ClientSize.Width)
+            {
+                speed.X *= -1;
+                l_score++;
             }
             if (intersects(ball, leftPaddle))
             {
-             
+
+                //speed.X *= -1;
                 speed.X = Math.Abs(speed.X);
             }
-            if (intersects(ball, rightPaddle))
+            else if (intersects(ball, rightPaddle))
             {
-                
+
+                //speed.X *= -1;
                 speed.X = -Math.Abs(speed.X);
             }
-            // paddles  
-            //if(ball.X <= leftPaddle.Right)
-            //{
-            //    speed.X *= -1;
-            //}
-            //if(ball.Y <= leftPaddle.Height)
-            //{
-            //    //speed.Y *= -1;
-            //}
 
+            score_text_box.Text = $"            {l_score} | {r_score}";
 
-
-
-
+            if (r_score == winning_score)
+            {
+                score_text_box.Enabled = false;
+                score_text_textbox.Enabled = false;
+                speed.X = 0;
+                speed.Y = 0;
+            }
+            else if (l_score == winning_score)
+            {
+                score_text_box.Enabled = false;
+                score_text_textbox.Enabled = false;
+                speed.X = 30;
+                speed.Y = 30;
+            }
+            
+            if(pressedKeys.Contains(Keys.S))
+            {
+                leftPaddle.Y += 5;
+                //do shit
+            }
+            if(pressedKeys.Contains(Keys.W))
+            {
+                leftPaddle.Y -= 5;
+            }
+            if(pressedKeys.Contains(Keys.Down))
+            {
+                rightPaddle.Y += 5;
+            }
+            if(pressedKeys.Contains(Keys.Up))
+            {
+                rightPaddle.Y -= 5;
+            }
 
             pictureBox1.Image = bmp;
         }
@@ -101,15 +137,11 @@ namespace Pong
         private void Form1_KeyDown(object sender, KeyEventArgs e)
         {
             var keyPress = e.KeyCode;
-            if (keyPress == Keys.Down)
-            {
-                leftPaddle.Y += 5;
-            //do shit
-            }
-            if (keyPress == Keys.Up)
-            {
-                leftPaddle.Y -= 5;
-            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+
         }
     }
 }
